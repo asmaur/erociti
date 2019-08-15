46ef65ec7429fc1fbe7fbceaa0fb9e57c97cfb55
A. Maurice <maur.a@outlook.com>
2019-08-15 03:32:10 -0300

    citi build v1 refact vueitem

--- frontciti/src/genapi.js
+++ frontciti/src/genapi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8000/citi/',
-    //baseURL: 'https://api.erociti.com/citi/',
+    //baseURL: 'http://localhost:8000/citi/',
+    baseURL: 'https://api.erociti.com/citi/',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': "Token 2d63ded23fc2a5e9653620af32e5cefe5e278094"
